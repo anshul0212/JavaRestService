b54853403b34c4abcbe1ed96541d56cca9b18a6c
Anshul Arora <anch0212@gmail.com>
2015-05-07 17:17:57 +0530

    Json Data from MySql.
    Json Data from MySql.

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/25/f0b7690673f200141f60bee7d1ba714d
@@ -0,0 +1,25 @@
+package com.learning.testProject.status;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/v1/status")
+public class V1_status {
+	
+	private static final String api_version = "00.1";
+	
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnTitle()
+	{
+		return "<p>Java WS</p>";
+	}
+
+	@Path("/version")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnVersion()
+	{
+		return "<p>Version: "+api_version+"</p>";
+	}
+}
--- com.learning.testProject/src/com/learning/testProject/status/V1_status.java
+++ com.learning.testProject/src/com/learning/testProject/status/V1_status.java
@@ -1,7 +1,11 @@
 package com.learning.testProject.status;
 
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.*;
+
+import java.sql.*;
+
+import com.learning.testProject.dao.*;
 
 @Path("/v1/status")
 public class V1_status {
@@ -21,5 +25,46 @@ public class V1_status {
 	public String returnVersion()
 	{
 		return "<p>Version: "+api_version+"</p>";
+	}
+	
+	@Path("/database")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String getDatabaseStatus() throws Exception
+	{
+		PreparedStatement query = null;
+		String name = null, empNo = null,
+     		   returnString = null;
+		Connection conn = null;
+		
+		try
+		{
+			conn = MySqlDB.getMySqlDBCon().getConnection();
+			query = conn.prepareStatement("select Name, EmpNo from myFirstTestTable");
+			ResultSet rs = query.executeQuery();
+			
+			while(rs.next())
+			{
+				name = rs.getString("Name");
+				empNo = rs.getString("EmpNo");
+			}
+			
+			query.close();
+			returnString = "<p>myFirstTest Table:</p>" +
+							"<p>Name :"+ name +"</p>" +
+							"<p>Emp No. :"+ empNo +"</p>";
+			
+		}
+		catch(Exception e)
+		{
+			returnString = "<p>Exception generated while connecting to database.</p>";
+			e.printStackTrace();
+		}
+		finally
+		{
+			if(conn != null)
+			conn.close();
+		}
+		return returnString;
 	}
 }
